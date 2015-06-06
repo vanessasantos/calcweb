9fb303acfccc53d180b82a9c27439bc1e0888149
alexfdealmeida <alexferreiradealmeida@gmail.com>
2015-06-06 15:27:55 -0300

    Adição dos fontes do projeto calcweb.

---
+++ src/main/java/br/com/delta_tcc/bean/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.delta_tcc.bean;
+
+public class Calculadora {
+
+	private double n1;
+	private double n2;
+	private char operador;
+
+	public Calculadora() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Calculadora(double n1, double n2, char operador) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+		this.operador = operador;
+	}
+
+	public double getN1() {
+		return n1;
+	}
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+	public double getN2() {
+		return n2;
+	}
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+	public char getOperador() {
+		return operador;
+	}
+
+	public void setOperador(char operador) {
+		this.operador = operador;
+	}
+
+}
